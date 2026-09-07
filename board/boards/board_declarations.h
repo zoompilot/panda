f8abc1589aec3c3da69c9b830f323948eea60caf
Zeph Leggett <zephleggett@gmail.com>
2026-09-07 02:01:52 -0500

    restore dos board

--- board/boards/board_declarations.h
+++ board/boards/board_declarations.h
@@ -49,6 +49,7 @@ struct board {
 // ******************* Definitions ********************
 // These should match the enums in cereal/log.capnp and __init__.py
 #define HW_TYPE_UNKNOWN 0U
+#define HW_TYPE_DOS 6U
 #define HW_TYPE_RED_PANDA 7U
 #define HW_TYPE_TRES 9U
 #define HW_TYPE_CUATRO 10U
@@ -57,6 +58,7 @@ struct board {
 #define CAN_MODE_NORMAL 0U
 #define CAN_MODE_OBD_CAN2 1U
 
+extern struct board board_dos;
 extern struct board board_tres;
 extern struct board board_cuatro;
 extern struct board board_red;
